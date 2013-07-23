fce25f49a07fc815e5a42cc77888f3c6fff36d72
Owen Holloway <owen.holloway101@gmail.com>
2013-07-23 22:40:43 +1000

    Lets build a nice UI

---
+++ FromScratch/src/tk/zeryter/main/GUI/Canvas.java
@@ -0,0 +1,15 @@
+package tk.zeryter.main.GUI;
+
+import java.awt.*;
+
+public class Canvas extends java.awt.Canvas implements Runnable {
+
+    public void run(){
+
+    }
+
+    public void paint(Graphics g){
+        g.drawLine(10,10,40,40);
+    }
+
+}
