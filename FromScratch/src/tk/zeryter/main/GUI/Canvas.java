5c9523a8140b5d7a08a8fd7190a4536d8445a32c
Owen Holloway <owen.holloway101@gmail.com>
2013-07-24 10:11:06 +1000

    Images are a dick.

--- FromScratch/src/tk/zeryter/main/GUI/Canvas.java
+++ FromScratch/src/tk/zeryter/main/GUI/Canvas.java
@@ -1,18 +1,13 @@
 package tk.zeryter.main.GUI;
 
 import java.awt.*;
-import java.awt.image.*;
 
 public class Canvas extends java.awt.Canvas {
 
-    Image test;
-
     public void paint(Graphics g){
 
-        test = Toolkit.getDefaultToolkit().getImage("assets/test.png");
+        g.fillRect(0,0,getWidth(),getHeight()/2);
 
-        g.drawImage(test,10,10,this);
-        g.drawLine(0,getHeight()/2,getWidth(),getHeight()/2);
     }
 
 }
